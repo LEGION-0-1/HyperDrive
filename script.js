f925af61050ab4240f415bb3a126417ec9193610
LEGION-0-1 <the.dilpreet.1@gmail.com>
2024-08-04 19:50:37 +0530

    Scroll progress bar fully functional

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function darkMode() {
 const bodyEl = document.body;
 const barEl = document.querySelector('.bar');
 const updateBar = () => {
-    let scrollPos = (window.scrollY / (bodyEl.scrollHeight - window.innerHeight))100;
+    let scrollPos = (window.scrollY / (bodyEl.scrollHeight - window.innerHeight)) * 100;
     barEl.style.width = `${scrollPos}%`;
     requestAnimationFrame(updateBar);
 };
